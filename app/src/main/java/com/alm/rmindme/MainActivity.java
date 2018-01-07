35b272fba5a644b38523fc37842b7ba885bbff11
leon <lyanovs@gmail.com>
2018-01-07 23:54:04 +0200

    [REMINDME-4]Implemented Toolbar

--- app/src/main/java/com/alm/rmindme/MainActivity.java
+++ app/src/main/java/com/alm/rmindme/MainActivity.java
@@ -1,8 +1,12 @@
 package com.alm.rmindme;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.alm.rmindme.R;
 
@@ -11,10 +15,27 @@ import com.alm.rmindme.R;
  */
 
 public class MainActivity extends Activity{
+    private Toolbar toolbar;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolbar();
+
+    }
+
+
+    @SuppressLint("ResourceType")
+    private void initToolbar(){
+        toolbar=(Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
 
     }
 }
