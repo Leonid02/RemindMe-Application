d36e1c1c121059f423c601de8697915b5525c02e
leon <lyanovs@gmail.com>
2018-01-07 18:43:53 +0200

    some updates

---
+++ app/src/main/java/MainActivity.java
@@ -0,0 +1,18 @@
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.alm.rmindme.R;
+
+/**
+ * Created by user on 1/7/2018.
+ */
+
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
