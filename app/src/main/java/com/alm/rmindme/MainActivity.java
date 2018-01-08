bf44f844031243a8f7d5b1160149047cf6196a61
leon <lyanovs@gmail.com>
2018-01-08 16:27:28 +0200

    [REMINDME-4]Implemented Toolbar

--- app/src/main/java/com/alm/rmindme/MainActivity.java
+++ app/src/main/java/com/alm/rmindme/MainActivity.java
@@ -1,23 +1,21 @@
 package com.alm.rmindme;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-
-import com.alm.rmindme.R;
 
 /**
  * Created by user on 1/7/2018.
  */
 
-public class MainActivity extends Activity{
+public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initToolbar();
